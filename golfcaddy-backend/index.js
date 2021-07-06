d914e838ce0c91130c66244d96e1591b6a0a3b3e
Tuukka Pitkanen <tuukkapitkanen2@gmail.com>
2021-07-06 13:30:38 +0300

    get ja post alustavat toteutukset

--- golfcaddy-backend/index.js
+++ golfcaddy-backend/index.js
@@ -4,8 +4,8 @@ Tällä hetkellä expressin dokumentaation
 aloitusesimerkki https://expressjs.com/en/starter/hello-world.html
 
 */
-const express = require('express')
-const mongoose = require('mongoose')
+const express = require('express') // http-sovelluksen toteuttamiseen
+const mongoose = require('mongoose') // tietokanta yhteyden kontrollointiin
 require('dotenv').config() // dotenv:in avulla ei tarvitse komentoriviltä välittää tietokannan kirjautumistietoja vaan ".env"-tiedostosta
 
 // Tietokanta
@@ -29,30 +29,45 @@ const Course = mongoose.model('Course', courseSchema)
 const app = express() // express-instanssi
 const port = 3000 // portin numero
 
-// eri http-pyyntöjen käsittelijät
-app.get('/', (req, res) => {
-    //luodaan testirata
-    const courseSippulanniemi = new Course({
-        name: 'Jyväs-Golf',
-        pars: [
-            { par: 3 },
-            { par: 4 },
-            { par: 4 },
-            { par: 4 },
-            { par: 4 },
-            { par: 3 },
-            { par: 4 },
-            { par: 4 },
-            { par: 5 },
-        ],
-    })
-
-    //tallennetaan rata tietokantaan ja palautetaan
-    courseSippulanniemi.save().then(() => res.send(courseSippulanniemi))
+//luodaan testirata
+const courseSippulanniemi = new Course({
+    name: 'Jyväs-Golf',
+    pars: [
+        { par: 3 },
+        { par: 4 },
+        { par: 4 },
+        { par: 4 },
+        { par: 4 },
+        { par: 3 },
+        { par: 4 },
+        { par: 4 },
+        { par: 5 },
+    ],
 })
 
-app.post('/', (req, res) => {
-    res.send('Got a POST request')
+//tallennetaan rata tietokantaan ja palautetaan
+courseSippulanniemi.save()
+
+// eri http-pyyntöjen käsittelijät
+app.get('/', async (req, res) => {
+    // courses sisältää kaikki tietokannassa olevat radat
+    const courses = await Course.find({})
+    res.json(courses)
+})
+
+app.get('/:id', async (req, res) => {
+    // course sisältää id:tä vastaavan radan
+    const course = await Course.findOne({ _id: req.params.id })
+    res.json(course)
+})
+
+app.post('/', async (req, res) => {
+    // luodaan pyynnön mukana tulleesta oliosta rata
+    const newCourse = new Course(req.body)
+    // tallennetaan, varmistetaan että tallennus on ohi
+    const savedCourse = await newCourse.save()
+    // vastataan pyytäjälle
+    res.status(201).json(savedCourse)
 })
 
 app.put('/user', (req, res) => {
